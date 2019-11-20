cd4f6adddfc495faa33f2fda789c3bf7ea38e27f
Cal Wilson <calwilson@Cals-MacBook-Pro.local>
2019-11-19 20:22:11 -0700

    DB Insert test

--- src/buisness/dogs.js
+++ src/buisness/dogs.js
@@ -14,9 +14,12 @@ const updateDog = (req, res) => {
 
 const createDog = (req, res) => {
     const { owner_id, name, type, age, available, price, color, weight, height } = req.body;
-    const validated = owner_id && name && type && age && available && price && color && weight && height;
+    const insertQuery = `INSERT INTO dog(owner_id, dog_name, dog_type, age, available, price, color, dog_weight, dog_height) VALUES(${owner_id}, '${name}', '${type}', ${age}, ${available}, ${price}, '${color}', ${weight}, ${height})`;
+    dbRequest(insertQuery);
+    const getQuery = 'SELECT * FROM dog';
+    const dogs = dbRequest(getQuery);
     res.contentType('application/json');
-    res.send(JSON.stringify({ validated }));
+    res.send(JSON.stringify(dogs));
 };
 
 const deleteDog = (req, res) => {
