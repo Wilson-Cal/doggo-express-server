914b898104758973f537a32c2f41e3b00f42cce2
Cal Wilson <calwilson@Cals-MacBook-Pro.local>
2019-11-19 20:28:33 -0700

    Test 2

--- src/buisness/dogs.js
+++ src/buisness/dogs.js
@@ -15,11 +15,17 @@ const updateDog = (req, res) => {
 const createDog = (req, res) => {
     const { owner_id, name, type, age, available, price, color, weight, height } = req.body;
     const insertQuery = `INSERT INTO dog(owner_id, dog_name, dog_type, age, available, price, color, dog_weight, dog_height) VALUES(${owner_id}, '${name}', '${type}', ${age}, ${available}, ${price}, '${color}', ${weight}, ${height})`;
-    dbRequest(insertQuery);
-    const getQuery = 'SELECT * FROM dog';
-    const dogs = dbRequest(getQuery);
-    res.contentType('application/json');
-    res.send(JSON.stringify(dogs));
+    try {
+        dbRequest(insertQuery);
+        const getQuery = 'SELECT * FROM dog';
+        const dogs = dbRequest(getQuery);
+        res.contentType('application/json');
+        res.send(JSON.stringify(dogs));
+    } catch (err) {
+        res.contentType('application/json');
+        res.sendStatus(500);
+        res.send(JSON.stringify(err));
+    }
 };
 
 const deleteDog = (req, res) => {
