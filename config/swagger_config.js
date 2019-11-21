05636bab1a537e24b753edea278b77ab7cfee00f
Cal Wilson <calwilson@Cals-MacBook-Pro.local>
2019-11-20 21:55:29 -0700

    quick fix

--- config/swagger_config.js
+++ config/swagger_config.js
@@ -5,7 +5,7 @@ const swagger_config = {
             title: 'Doggo API',
             version: '0.1.0',
         },
-        host: 'https://doggo-express-server.herokuapp.com/',
+        host: 'doggo-express-server.herokuapp.com/',
         basePath: '/api/v1',
         produces: [
             'application/json'
