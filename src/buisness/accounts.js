24469bacb9e293b4c78c15ca035444ade76d53d4
Cal Wilson <calwilson@Cals-MacBook-Pro.local>
2019-11-20 18:43:12 -0700

    users create endpoint

--- src/buisness/accounts.js
+++ src/buisness/accounts.js
@@ -31,8 +31,4 @@ const login = async (req, res) => {
     }
 };
 
-const signup = async (req, res) => {
-
-};
-
 module.exports = { login };
--- src/buisness/users.js
+++ src/buisness/users.js
@@ -1,6 +1,10 @@
+const bcrypt = require('bcrypt');
+
 const dbRequest = require('./db_request.js');
 
-const getUser = (req, res) => {
+const saltRounds = 10;
+
+const getUser = async (req, res) => {
     const { id } = req.params;
     res.contentType('application/json');
     res.send(JSON.stringify({ id }));
@@ -12,10 +16,20 @@ const updateUser = (req, res) => {
     res.send(JSON.stringify({ id }));
 };
 
-const createUser = (req, res) => {
-    const { id } = req.body;
-    res.contentType('application/json');
-    res.send(JSON.stringify({ id }));
+const createUser = async (req, res) => {
+    const { username, email, password } = req.body;
+    try {
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        const insertQuery = `INSERT INTO user(username, email, user_password) VALUES('${username}', '${email}', '${hashedPassword}') RETURNING id, username, email`;
+        const user = await dbRequest(insertQuery);
+        res.contentType('application/json');
+        res.send(JSON.stringify(user));
+    } catch (err) {
+        res.contentType('application/json');
+        res.sendStatus(500);
+        res.send(JSON.stringify(err));
+    }
+
 };
 
 const deleteUser = (req, res) => {
