afc5a84d5cf50c7652203412c315fb7b6b3f899a
Cal Wilson <calwilson@Cals-MacBook-Pro.local>
2019-12-04 18:02:10 -0700

    quick check

--- src/buisness/accounts.js
+++ src/buisness/accounts.js
@@ -5,31 +5,33 @@ const { createUser } = require('./users.js');
 const dbRequest = require('../utilities/db_request.js');
 
 const login = async (req, res) => {
-    const { email, password } = req.body;
-    const getQuery = `GET password FROM user WHERE email = ${email}`;
-    const hashedPassword = await dbRequest(getQuery);
-    if (hashedPassword) {
-        // Check Hashed password against sent password
-        const match = await bcrypt.compare(password, hashedPassword);
-        if (match) {
-            // Generate temp cookie value
-            const cookieValue = uuidAPIKey.create();
-            // Write the temp cookie value to the database
-            const updateQuery = `UPDATE user_app SET temp_auth = ${cookieValue.uuid} WHERE email=${email}`;
-            await dbRequest(updateQuery);
-            // Send success to the user
-            res.contentType('application/json');
-            res.send(JSON.stringify({ match, cookieValue: cookieValue.apiKey }));
-        } else {
-            // Send failure to user
-            res.contentType('application/json');
-            res.send(JSON.stringify({ match }));
-        }
-    } else {
-        // Send failure to user
-        res.contentType('application/json');
-        res.send({ match: false });
-    }
+    // const { email, password } = req.body;
+    // const getQuery = `GET password FROM user WHERE email = ${email}`;
+    // const hashedPassword = await dbRequest(getQuery);
+    // if (hashedPassword) {
+    //     // Check Hashed password against sent password
+    //     const match = await bcrypt.compare(password, hashedPassword);
+    //     if (match) {
+    //         // Generate temp cookie value
+    //         const cookieValue = uuidAPIKey.create();
+    //         // Write the temp cookie value to the database
+    //         const updateQuery = `UPDATE user_app SET temp_auth = ${cookieValue.uuid} WHERE email=${email}`;
+    //         await dbRequest(updateQuery);
+    //         // Send success to the user
+    //         res.contentType('application/json');
+    //         res.send(JSON.stringify({ match, cookieValue: cookieValue.apiKey }));
+    //     } else {
+    //         // Send failure to user
+    //         res.contentType('application/json');
+    //         res.send(JSON.stringify({ match }));
+    //     }
+    // } else {
+    //     // Send failure to user
+    //     res.contentType('application/json');
+    //     res.send({ match: false });
+    // }
+    res.contentType('application/json');
+    res.send(JSON.stringify({ Hello: 'There' }));
 };
 
 const signup = async (req, res) => {
